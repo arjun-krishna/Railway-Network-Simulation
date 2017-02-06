d840280b91c36eebefd3ef0f6a7c1d2616de5804
suhaschintala <suhaschintala@gmail.com>
2017-02-06 10:13:22 +0530

    platform_num getter from indiarailinfo

---
+++ model/get_platform_no.py
@@ -0,0 +1,42 @@
+"""
+<span id="InputVars" style="display:none">LappGetStationList/howra/0/1/0?&amp;date=1485936397031&amp;nextListSeqNumber=0</span>
+<table class="dropdowntable" numRows="10" cellspacing=0><tr class=rowM1 rowNum="0">
+<td style="display:none">1</td><td class=rcol nowrap><span>HWH</span></td>
+<td class=icol nowrap><span><span class=listmatch>Howra</span>h Jn</span></td>
+<td class=jcol nowrap><span>ER-<span class=listmatch>Howra</span>h Div</span></td>
+<td style="display:none;">HWH/Howrah Junction</td>
+</tr>
+<tr class=rowm2 rowNum="0"><td style="display:none">1</td><td><span nowrap>&nbsp;</span></td><td colspan=2 nowrap><span><span class=listmatch>Howra</span>h 711101 West Bengal</span></td><td style="display:none">HWH/Howrah Junction</td></tr>
+<tr class=rowM1 rowNum="1"><td style="display:none">325</td><td class=rcol nowrap><span>SDAH</span></td><td class=icol nowrap><span>Sealdah</span></td><td class=jcol nowrap><span>ER-Sealdah Div</span></td><td style="display:none;">SDAH/Sealdah</td></tr>
+<tr class=rowm2 rowNum="1"><td style="display:none">325</td><td><span nowrap>&nbsp;</span></td><td colspan=2 nowrap><span>Kolkata 700014 West Bengal</span></td><td style="display:none">SDAH/Sealdah</td></tr><tr class=rowM1 rowNum="2"><td style="display:none">7037</td><td class=rcol nowrap><span>KOAA</span></td><td class=icol nowrap><span>Kolkata</span></td><td class=jcol nowrap><span>ER-Sealdah Div</span></td><td style="display:none;">KOAA/Kolkata</td></tr><tr class=rowm2 rowNum="2"><td style="display:none">7037</td><td><span nowrap>&nbsp;</span></td><td colspan=2 nowrap><span>Kolkata Ph:033-27050307 West Bengal</span></td><td style="display:none">KOAA/Kolkata</td></tr><tr class=rowM1 rowNum="3"><td style="display:none">1724</td><td class=rcol nowrap><span>SRC</span></td><td class=icol nowrap><span>Santragachi Jn</span></td><td class=jcol nowrap><span>SER-Kharagpur Div</span></td><td style="display:none;">SRC/Santragachi Junction</td></tr><tr class=rowm2 rowNum="3"><td style="display:none">1724</td><td><span nowrap>&nbsp;</span></td><td colspan=2 nowrap><span>Dist: <span class=listmatch>Howra</span>h - 711104 West Bengal</span></td><td style="display:none">SRC/Santragachi Junction</td></tr><tr class=rowM1 rowNum="4"><td style="display:none">581</td><td class=rcol nowrap><span>BDC</span></td><td class=icol nowrap><span>Bandel Jn</span></td><td class=jcol nowrap><span>ER-<span class=listmatch>Howra</span>h Div</span></td><td style="display:none;">BDC/Bandel Junction</td></tr><tr class=rowm2 rowNum="4"><td style="display:none">581</td><td><span nowrap>&nbsp;</span></td><td colspan=2 nowrap><span>Dist - Hooghly 712123 West Bengal</span></td><td style="display:none">BDC/Bandel Junction</td></tr><tr class=rowM1 rowNum="5"><td style="display:none">958</td><td class=rcol nowrap><span>NH</span></td><td class=icol nowrap><span>Naihati Jn</span></td><td class=jcol nowrap><span>ER-Sealdah Div</span></td><td style="display:none;">NH/Naihati Junction</td></tr><tr class=rowm2 rowNum="5"><td style="display:none">958</td><td><span nowrap>&nbsp;</span></td><td colspan=2 nowrap><span>dist - North 24 paraganas - 743165 Ph: 033-25812128 West Bengal</span></td><td style="display:none">NH/Naihati Junction</td></tr><tr class=rowM1 rowNum="6"><td style="display:none">1913</td><td class=rcol nowrap><span>SHM</span></td><td class=icol nowrap><span>Kolkata Shalimar</span></td><td class=jcol nowrap><span>SER-Kharagpur Div</span></td><td style="display:none;">SHM/Kolkata Shalimar</td></tr><tr class=rowm2 rowNum="6"><td style="display:none">1913</td><td><span nowrap>&nbsp;</span></td><td colspan=2 nowrap><span>Dist - <span class=listmatch>Howra</span>h West Bengal</span></td><td style="display:none">SHM/Kolkata Shalimar</td></tr><tr class=rowM1 rowNum="7"><td style="display:none">2656</td><td class=rcol nowrap><span>BP</span></td><td class=icol nowrap><span>Barrackpore</span></td><td class=jcol nowrap><span>ER-Sealdah Div</span></td><td style="display:none;">BP/Barrackpore</td></tr><tr class=rowm2 rowNum="7"><td style="display:none">2656</td><td><span nowrap>&nbsp;</span></td><td colspan=2 nowrap><span>Dist - 24 paraganas (N) West Bengal</span></td><td style="display:none">BP/Barrackpore</td></tr><tr class=rowM1 rowNum="8"><td style="display:none">2045</td><td class=rcol nowrap><span>ULB</span></td><td class=icol nowrap><span>Uluberia</span></td><td class=jcol nowrap><span>SER-Kharagpur Div</span></td><td style="display:none;">ULB/Uluberia</td></tr><tr class=rowm2 rowNum="8"><td style="display:none">2045</td><td><span nowrap>&nbsp;</span></td><td colspan=2 nowrap><span>Dist - <span class=listmatch>Howra</span>h 711315 West Bengal</span></td><td style="display:none">ULB/Uluberia</td></tr><tr class=rowM1 rowNum="9"><td style="display:none">2053</td><td class=rcol nowrap><span>BZN</span></td><td class=icol nowrap><span>Bagnan</span></td><td class=jcol nowrap><span>SER-Kharagpur Div</span></td><td style="display:none;">BZN/Bagnan</td></tr><tr class=rowm2 rowNum="9"><td style="display:none">2053</td><td><span nowrap>&nbsp;</span></td><td colspan=2 nowrap><span>Dist - <span class=listmatch>Howra</span>h West Bengal</span></td><td style="display:none">BZN/Bagnan</td></tr><tr><td class=listfooter colspan=5><span width=10% class=listfooterhidden>prev</span>&nbsp;&nbsp;<span width=10% class=listfootervisible>next</span>&nbsp;&nbsp;</td></tr><tr><td>&nbsp;</td></tr></table>
+
+"""
+'''
+https://indiarailinfo.com/shtml/list.shtml?LappGetStationList/che/0/0/0?&date=1486353447682&nextListSeqNumber=3
+'''
+
+import requests 
+from bs4 import BeautifulSoup as BS
+def get_platformnum_from_ir(station_name):
+	r = requests.get('http://indiarailinfo.com/shtml/list.shtml?LappGetStationList/' + station_name + '/0/0/0?')
+	bs = BS(r.text, 'html.parser')
+	table = bs.findAll('table')
+	if len(table) > 0: 
+		table = table[0]
+		tr = table.findAll('tr')
+		if len(tr) > 0:
+			tr = tr[0]
+			num = tr.findAll('td')[0].string
+			r = requests.get('http://indiarailinfo.com/arrivals/' + num + '?')
+			bs = BS(r.text, 'html.parser')
+			div = bs.findAll('div', {'class':'topcapsule'})[0]
+			h1 = div.find('h1')
+			text = h1.text
+			if 'PFs' in text :
+				pfnum = int(text.split(' PFs')[0].split('(')[1])
+				return {'data' : pfnum}
+			else :
+				return {'error' : 'no platform number in html'}
+
+string = raw_input('enter name \n')
+get_platformnum_from_ir(string)
